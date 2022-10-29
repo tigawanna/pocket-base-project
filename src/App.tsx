5ba34de2c1e274c920d6c41236048621ecfb132f
tigawanna <denniskinuthiaw@gmail.com>
2022-10-29 08:38:48 +0300

    addedctest mode in protected rutes

--- src/App.tsx
+++ src/App.tsx
@@ -6,9 +6,9 @@ import "../node_modules/table-for-react/dist/tailwind.css";
 import { Test } from './components/test/Test';
 
 
-import { client, getUser } from "./pocketbase/config";
+import { appendToCache, client, getUser } from "./pocketbase/config";
 
-import { useQuery } from "@tanstack/react-query";
+import { useQuery,useQueryClient } from "@tanstack/react-query";
 import { Protected } from './pocketbase/auth/Protected';
 import { Redirect } from "./pocketbase/auth/Redirect";
 import { Shops } from "./components/shops/Shops";
@@ -25,10 +25,30 @@ import { Tenant } from "./components/Tenants/Tenant";
 
 function App() {
 
-
+const querClient = useQueryClient()
 const userQuery = useQuery(["user"], getUser);
-
-
+const testmode= true
+  const dummyuser = {
+    "id": "20dq1d1c3h5mncl",
+    "created": "2022-09-18 06:12:51.080",
+    "updated": "2022-09-18 06:12:51.080",
+    "email": "denniskinuthiaw@gmail.com",
+    "verified": true,
+    "lastResetSentAt": "",
+    "lastVerificationSentAt": "",
+    "profile": {
+      "id": "fzezpytbmw2cb1e",
+      "created": "2022-09-18 06:12:51.081",
+      "updated": "2022-10-17 15:45:58.593",
+      "@collectionId": "systemprofiles0",
+      "@collectionName": "profiles",
+      "avatar": "",
+      "avatarUrl": "https://lh3.googleusercontent.com/a/ALm5wu0eTxuzjH5YNIzy1z46o25PonSNVXxyRomY8--qxPY=s96-c",
+      "name": "Dennis",
+      "userId": "20dq1d1c3h5mncl",
+      "@expand": {}
+    }
+  }
 //  console.log("auth === ",userQuery)
 
 if (userQuery.isLoading ) {
@@ -49,6 +69,10 @@ if (userQuery.isLoading ) {
     ); 
   }
   const user = userQuery.data;
+  if(!user){
+    appendToCache(['user'], querClient, dummyuser)
+  }
+
   // if(!user){
   //   return (
   //     <div className="w-full h-screen flex-center scroll-bar">
@@ -68,7 +92,7 @@ if (userQuery.isLoading ) {
             <Route
               path="/"
               element={
-                <Protected user={user}>
+                <Protected user={user} testmode={testmode}>
                   <Home user={user} />
                 </Protected>
               }
@@ -78,7 +102,7 @@ if (userQuery.isLoading ) {
             <Route
               path="/shops"
               element={
-                <Protected user={user}>
+                <Protected user={user} testmode={testmode}>
                   <Shops user={user} />
                 </Protected>
               }
@@ -86,7 +110,7 @@ if (userQuery.isLoading ) {
             <Route
               path="/tenants"
               element={
-                <Protected user={user}>
+                <Protected user={user} testmode={testmode}>
                   <Tenants user={user} />
                 </Protected>
               }
@@ -94,7 +118,7 @@ if (userQuery.isLoading ) {
             <Route
               path="/tenant/:tenantId"
               element={
-                <Protected user={user}>
+                <Protected user={user} testmode={testmode}>
                   <Tenant user={user} />
                 </Protected>
               }
@@ -103,7 +127,7 @@ if (userQuery.isLoading ) {
             <Route
               path="/shop"
               element={
-                <Protected user={user}>
+                <Protected user={user} testmode={testmode}>
                   <Shop user={user} floor={"ground"} shopId={"G-01"} />
                 </Protected>
               }
@@ -111,7 +135,7 @@ if (userQuery.isLoading ) {
             <Route
               path="/payments"
               element={
-                <Protected user={user}>
+                <Protected user={user} testmode={testmode}>
                   <Payment user={user} />
                 </Protected>
               }
--- src/pocketbase/auth/Protected.tsx
+++ src/pocketbase/auth/Protected.tsx
@@ -4,11 +4,12 @@ import { Navigate } from 'react-router-dom';
 
 interface ProtectedProps {
 user?: User | Admin | null
+testmode?:boolean
 children:ReactNode
 }
 
-export const Protected: React.FC<ProtectedProps> = ({user,children}) => {
-if(!user?.email){
+export const Protected: React.FC<ProtectedProps> = ({user,children,testmode}) => {
+if(!user?.email && !testmode){
  return <Navigate to={'/login'} />
 }
 return (
